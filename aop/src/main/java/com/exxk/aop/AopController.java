a7e822c6d79aeaad5ff7b91bba484df33bddde03
lx <exxk.lx@gmail.com>
2019-01-16 10:36:37 +0800

    添加shiro项目

--- aop/src/main/java/com/exxk/aop/AopController.java
+++ aop/src/main/java/com/exxk/aop/AopController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.Random;
 import java.util.logging.Logger;
 
 @RestController
@@ -15,6 +17,24 @@ public class AopController {
     @Log(tag = "我是注解")
     public String aop(){
         logger.info("业务代码执行中...");
+
+        double bj=10000;
+        double db=1000;
+        double ds=0.01;
+        int day=100;
+        double dz;
+        double tr=bj;
+        for (int i = 0; i < day; i++) {
+           dz=Math.random()>0.5?1:-1;
+            Random random=new Random();
+            ds=random.nextInt(10)*dz;
+            logger.info("随机点："+ds/100);
+            bj=bj+(bj*ds/100)-db*dz;
+            tr=tr-db*dz;
+            logger.info("总资产："+bj+"，总投入："+tr);
+        }
+
+
         return "aop 测试";
     }
 }
--- aop/src/test/java/com/exxk/aop/AopApplicationTests.java
+++ aop/src/test/java/com/exxk/aop/AopApplicationTests.java
@@ -11,7 +11,34 @@ public class AopApplicationTests {
 
     @Test
     public void contextLoads() {
+        Integer numAobj=new Integer(10);
+        Integer numBobj=new Integer(20);
+        int numA=10;
+        int numB=20;
+
+        swap(numA,numB);
+        swapObj(numAobj,numBobj);
+        System.out.println("numA="+numA);
+        System.out.println("numB="+numB);
+        System.out.println("numAobj="+numAobj);
+        System.out.println("numBobj="+numBobj);
+
     }
 
+    public void swapObj(Integer a,Integer b){
+        Integer temp=a;
+        a=b;
+        b=temp;
+        System.out.println("aObj="+a);
+        System.out.println("bObj="+b);
+    }
+
+    public void swap(int a,int b){
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
 }
 
