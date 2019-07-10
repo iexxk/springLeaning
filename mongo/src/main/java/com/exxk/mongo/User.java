a69743812040ba9bbbf4e67cf75672917366c128
xuanleung <exxk.lx@gmail.com>
2019-07-10 14:35:08 +0800

    添加 mongo change stream 测试

---
+++ mongo/src/main/java/com/exxk/mongo/User.java
@@ -0,0 +1,39 @@
+package com.exxk.mongo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "user_info")
+public class User {
+    @Id
+    private String id;
+    @Field("name")
+    private String name;
+    @Field("age")
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- mongo/src/test/java/com/exxk/mongo/MongoApplicationTests.java
+++ mongo/src/test/java/com/exxk/mongo/MongoApplicationTests.java
@@ -2,15 +2,56 @@ package com.exxk.mongo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.ChangeStreamEvent;
+import org.springframework.data.mongodb.core.ChangeStreamOptions;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MongoApplicationTests {
 
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Autowired
+    ReactiveMongoTemplate reactiveMongoTemplate;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void addUser(){
+        User user=new User();
+        user.setAge(18);
+        user.setName("Lisa");
+        mongoTemplate.save(user);
+    }
+
+    @Test
+    public void selectUser() throws InterruptedException {
+        List<User> userList= mongoTemplate.findAll(User.class);
+        System.out.println("用户数量："+userList.size());
+
+
+        ChangeStreamOptions options= ChangeStreamOptions.builder().filter().build();
+        Flux<ChangeStreamEvent<User>> flux = reactiveMongoTemplate.changeStream("user", options, User.class);
+
+        flux.subscribe(userChangeStreamEvent -> {
+            userChangeStreamEvent.getBody();
+        });
+        TimeUnit.MINUTES.sleep(5);
+
     }
 
 }
