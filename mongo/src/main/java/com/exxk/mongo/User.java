bee75a5b60835ecfcf3f0006d5e47c338e370b97
xuanleung <exxk.lx@gmail.com>
2019-08-28 17:41:27 +0800

    添加 mongo  测试

--- mongo/src/main/java/com/exxk/mongo/User.java
+++ mongo/src/main/java/com/exxk/mongo/User.java
@@ -1,39 +1,50 @@
 package com.exxk.mongo;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+import java.util.Map;
+
 
 @Document(collection = "user_info")
 public class User {
     @Id
     private String id;
-    @Field("name")
-    private String name;
-    @Field("age")
-    private Integer age;
 
-    public String getId() {
-        return id;
+    @Indexed()
+    @Field("num")
+    private String num;
+
+    @Field("user_map")
+    private Map<String,BaseUser> userMap;
+
+    @Field("user_list")
+    private List<BaseUser> userList;
+
+    public String getNum() {
+        return num;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setNum(String num) {
+        this.num = num;
     }
 
-    public String getName() {
-        return name;
+    public Map<String, BaseUser> getUserMap() {
+        return userMap;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserMap(Map<String, BaseUser> userMap) {
+        this.userMap = userMap;
     }
 
-    public Integer getAge() {
-        return age;
+    public List<BaseUser> getUserList() {
+        return userList;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
+    public void setUserList(List<BaseUser> userList) {
+        this.userList = userList;
     }
 }
--- mongo/src/test/java/com/exxk/mongo/MongoApplicationTests.java
+++ mongo/src/test/java/com/exxk/mongo/MongoApplicationTests.java
@@ -1,20 +1,18 @@
 package com.exxk.mongo;
 
+import com.mongodb.BasicDBObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.ChangeStreamEvent;
-import org.springframework.data.mongodb.core.ChangeStreamOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.FluxExchangeResult;
-import reactor.core.publisher.Flux;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,27 +29,137 @@ public class MongoApplicationTests {
     }
 
     @Test
-    public void addUser(){
-        User user=new User();
+    public void addBaseUser(){
+        BaseUser user=new BaseUser();
         user.setAge(18);
         user.setName("Lisa");
         mongoTemplate.save(user);
     }
 
     @Test
-    public void selectUser() throws InterruptedException {
-        List<User> userList= mongoTemplate.findAll(User.class);
-        System.out.println("用户数量："+userList.size());
-
-
-        ChangeStreamOptions options= ChangeStreamOptions.builder().filter().build();
-        Flux<ChangeStreamEvent<User>> flux = reactiveMongoTemplate.changeStream("user", options, User.class);
-
-        flux.subscribe(userChangeStreamEvent -> {
-            userChangeStreamEvent.getBody();
-        });
-        TimeUnit.MINUTES.sleep(5);
+    public void addUser(){
+        User user=new User();
+        BaseUser baseUser=new BaseUser();
+        baseUser.setAge(18);
+        baseUser.setName("Lisa");
+        Map<String,BaseUser> baseUserMap=new HashMap<>();
+        baseUserMap.put(baseUser.getName(),baseUser);
+        user.setUserMap(baseUserMap);
+        user.setNum("1");
+        mongoTemplate.save(user);
 
     }
+    @Test
+    public void selectUser(){
+        User user=new User();
+        BaseUser baseUser=new BaseUser();
+        baseUser.setAge(18);
+        baseUser.setName("Lisa");
+        Map<String,BaseUser> baseUserMap=new HashMap<>();
+        baseUserMap.put(baseUser.getName(),baseUser);
+        user.setUserMap(baseUserMap);
+        user.setNum("1");
+        Query query= new Query();
+//        query.addCriteria(new Criteria().andOperator(Criteria.where("num").is("1")
+//                ,Criteria.where("user_list.name").ne("lisa")));
+        Criteria criteria=new Criteria();
+        criteria.and("num").is("1");
+        criteria.and("user_list.name").ne("lisa");
+        query.addCriteria(criteria);
+//        query.addCriteria(Criteria.where("num").is("1"));
+//        query.addCriteria(Criteria.where("user_list.name").ne("lisa"));
+
+        Update update=new Update();
+
+
+        BaseUser baseUser2=new BaseUser();
+        baseUser2.setName("lisa");
+        baseUser2.setAge(20);
+        update.addToSet("user_list",baseUser2);
+        mongoTemplate.upsert(query,update,User.class);
+    }
+
+    @Test
+    public void aa(){
+
+    }
+
+    @Test
+    public void ss(){
+        BasicDBObject bson = new BasicDBObject();
+        bson.put("$eval","db.getCollection('user_info').update({'num':'1','user_list.name':{$ne:'kk'}},{$addToSet:{'user_list':{'name':'kk','age':1}}})");
+        mongoTemplate.getDb().runCommand(bson);
+    }
+
+
+    @Test
+    public void testList(){
+        User user=new User();
+
+        BaseUser baseUser=new BaseUser();
+        baseUser.setAge(18);
+        baseUser.setName("lisa");
+        List<BaseUser> baseUserList=new ArrayList<>();
+        baseUserList.add(baseUser);
+        user.setNum("1");
+        user.setUserList(baseUserList);
+
+        mongoTemplate.save(user);
+    }
+
+    @Test
+    public void update(){
+        Query query= new Query();
+//        query.addCriteria(new Criteria().andOperator(Criteria.where("num").is("1")
+//                ,Criteria.where("user_list.name").ne("lisa")));
+        Criteria criteria=new Criteria();
+        criteria.and("num").is("1");
+        criteria.and("dd").is("ss");
+        query.addCriteria(criteria);
+
+        Update update=new Update();
+
+
+        BaseUser baseUser2=new BaseUser();
+        baseUser2.setName("lisa");
+        baseUser2.setAge(20);
+        update.addToSet("user_list",baseUser2);
+        mongoTemplate.upsert(query,update,User.class);
+
+    }
+
+    @Test
+    public void addUser2(){
+//        User user=new User();
+//        BaseUser baseUser=new BaseUser();
+//        baseUser.setAge(18);
+//        baseUser.setName("Lisa1");
+//        List<BaseUser> baseUsers=new ArrayList<>();
+//        baseUsers.add(baseUser);
+//        user.setUsers(baseUsers);
+//        user.setNum("1");
+//
+//        Query query=new Query(Criteria.where("num").is(1));
+//        Update update = Update.update("users",baseUsers);
+//       // mongoTemplate.updateFirst(query, update, User.class);
+//        mongoTemplate.upsert(query, new Update().set("cname", "zcy"), User.class);
+
+    }
+
+//    @Test
+//    public void selectUser() throws InterruptedException {
+//        List<User> userList= mongoTemplate.findAll(User.class);
+//        System.out.println("用户数量："+userList.size());
+//
+//
+//        ChangeStreamOptions options= ChangeStreamOptions.builder().filter().build();
+//        Flux<ChangeStreamEvent<User>> flux = reactiveMongoTemplate.changeStream("user", options, User.class);
+//
+//        flux.subscribe(userChangeStreamEvent -> {
+//            userChangeStreamEvent.getBody();
+//        });
+//        TimeUnit.MINUTES.sleep(5);
+//
+//    }
 
 }
